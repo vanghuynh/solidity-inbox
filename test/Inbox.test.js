8c37eee00f9afac8298ff6d183b41ca0e5b8f700
vhuynh <vhuynh@gmail.com>
2023-12-04 11:12:31 +0700

    first unit test for smart contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,40 +1,41 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 
 // updated ganache and web3 imports added for convenience
-const assert = require('assert');
+const assert = require("assert");
 
 // instance web 3 need provider to connect local test network
 const web3 = new Web3(ganache.provider());
 
-
+// import interface and bytecode from compiled contract
+const { interface, bytecode } = require("../compile");
 
 // contract test code will go here
-class Car {
-    park() {
-        return 'stopped';
-    }
 
-    drive() {
-        return 'vroom';
-    }
+let accounts;
+let inbox;
 
-}
+beforeEach(async () => {
+  console.log("before each");
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
+  // Use one of those accounts to deploy the contract
+  // teach web3 about what methods an Inbox contract has
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    // tell web3 that we want to deploy a new copy of this contract
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    // instruct web3 to send out a transaction that creates this contract
+    .send({ from: accounts[0], gas: "1000000" });
+});
 
-let car;
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
 
-beforeEach(() => {
-    console.log('before each');
-    car = new Car();
-})
-
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
-    });
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
 });
