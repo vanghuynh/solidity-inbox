4f33e2e210560a6c6655ffa4c761ebb297156df7
vhuynh <vhuynh@gmail.com>
2023-12-05 10:34:13 +0700

    update project to latest solidity version

--- deploy.js
+++ deploy.js
@@ -1,27 +1,27 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const { Web3 } = require("web3");
-//updated web3 and hdwallet-provider imports added for convenience
-const { interface, bytecode } = require("./compile");
-
-// deploy code will go here
-// never put your private key here
-// just for test
+const { abi, evm } = require("./compile");
 
 const provider = new HDWalletProvider(
-  "win believe galaxy canoe mixed fatal mass huge xx xx xx xx",
-  "https://sepolia.infura.io/v3/xxxx"
+  //"REPLACE_WITH_YOUR_MNEMONIC",
+  "win believe galaxy canoe mixed fatal mass huge school alcohol arm ethics",
+  // remember to change this to your own phrase!
+  //"REPLACE_WITH_YOUR_INFURA_URL"
+  "https://sepolia.infura.io/v3/19e970920d9243d98a868b3dfa36df73"
+  // remember to change this to your own endpoint!
 );
-
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log("deploy from account: ", accounts[0]);
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
-    .send({ from: accounts[0], gas: "1000000" });
-  console.log("contract deployed to: ", result.options.address);
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
-
 deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,31 +1,21 @@
+const assert = require("assert");
 const ganache = require("ganache");
 const { Web3 } = require("web3");
-
-// updated ganache and web3 imports added for convenience
-const assert = require("assert");
-
-// instance web 3 need provider to connect local test network
 const web3 = new Web3(ganache.provider());
 
-// import interface and bytecode from compiled contract
-const { interface, bytecode } = require("../compile");
-
-// contract test code will go here
+const { abi, evm } = require("../compile");
 
 let accounts;
 let inbox;
 
 beforeEach(async () => {
-  console.log("before each");
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-
-  // Use one of those accounts to deploy the contract
-  // teach web3 about what methods an Inbox contract has
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    // tell web3 that we want to deploy a new copy of this contract
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
-    // instruct web3 to send out a transaction that creates this contract
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ["Hi there!"],
+    })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
@@ -33,12 +23,10 @@ describe("Inbox", () => {
   it("deploys a contract", () => {
     assert.ok(inbox.options.address);
   });
-
   it("has a default message", async () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, "Hi there!");
   });
-
   it("can change the message", async () => {
     await inbox.methods.setMessage("bye").send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
